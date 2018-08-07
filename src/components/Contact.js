6da538336aee1d0e9c146d77a850718bf1988e1a
Ketan Shukla <ketankshukla@gmail.com>
2018-08-07 11:50:17 -0700

    used destructuring in the Contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,13 +3,13 @@ import PropTypes from 'prop-types'
 
 class Contact extends Component {
   render() {
-    const { contact } = this.props
+    const { name, email, phone } = this.props.contact
     return (
       <div className="card card-body mb-3">
-        <h4>{contact.name}</h4>
+        <h4>{name}</h4>
         <ul className="list-group">
-          <li className="list-group-item">Email: {contact.email}</li>
-          <li className="list-group-item">Phone: {contact.phone}</li>
+          <li className="list-group-item">Email: {email}</li>
+          <li className="list-group-item">Phone: {phone}</li>
         </ul>
       </div>
     )
