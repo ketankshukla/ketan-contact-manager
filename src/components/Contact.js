91a0b824cb4a3d47aceed9fff1e345b540d8d3c2
Ketan Shukla <ketankshukla@gmail.com>
2018-08-07 12:59:16 -0700

    refactored showing of different arrow icons

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,49 +11,34 @@ class Contact extends Component {
   };
 
   onDeleteClick = e => {
-    console.log('clicked')
-  }
+    console.log('clicked');
+  };
 
   render() {
     const { name, email, phone } = this.props.contact;
     const { showContactInfo } = this.state;
     return (
       <div className="card card-body mb-3">
+        <h4>
+          {name}
+          <i
+            onClick={this.onShowClick}
+            className={showContactInfo ? "fa fa-angle-double-down" : "fa fa-angle-double-right"}
+            style={{ cursor: 'pointer' }}
+          />
+          <i
+            onClick={this.onDeleteClick}
+            className="fas fa-times"
+            style={{ cursor: 'pointer', float: 'right', color: 'red' }}
+          />
+        </h4>
+
         {showContactInfo ? (
-          <React.Fragment>
-            <h4>
-              {name}
-              <i
-                onClick={this.onShowClick}
-                className="fa fa-angle-double-down"
-                style={{ cursor: 'pointer' }}
-              />
-              <i
-              onClick={this.onDeleteClick}
-                className="fas fa-times"
-                style={{ cursor: 'pointer', float: 'right', color: 'red' }}
-              />
-            </h4>
-            <ul className="list-group">
-              <li className="list-group-item">Email: {email}</li>
-              <li className="list-group-item">Phone: {phone}</li>
-            </ul>
-          </React.Fragment>
-        ) : (
-          <h4>
-            {name}
-            <i
-              onClick={this.onShowClick}
-              className="fa fa-angle-double-right"
-              style={{ cursor: 'pointer' }}
-            />
-            <i
-                onClick={this.onDeleteClick}
-              className="fas fa-times"
-              style={{ cursor: 'pointer', float: 'right', color: 'red' }}
-            />
-          </h4>
-        )}
+          <ul className="list-group">
+            <li className="list-group-item">Email: {email}</li>
+            <li className="list-group-item">Phone: {phone}</li>
+          </ul>
+        ) : null}
       </div>
     );
   }
