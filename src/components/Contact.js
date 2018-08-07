f15382d43d058461e4bb27cab5605476d68b518b
Ketan Shukla <ketankshukla@gmail.com>
2018-08-07 12:37:54 -0700

    added dropdown logic with icons changing

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,30 +1,50 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Contact extends Component {
+  state = {
+    showContactInfo: false
+  };
 
-  state={}
-
-  onShowClick = (e) => {
-    
-  }
+  onShowClick = e => {
+    this.setState({ showContactInfo: !this.state.showContactInfo });
+  };
 
   render() {
-    const { name, email, phone } = this.props.contact
+    const { name, email, phone } = this.props.contact;
+    const { showContactInfo } = this.state;
     return (
       <div className="card card-body mb-3">
-        <h4>{name} <i onClick={this.onShowClick} className="fa fa-sort-down"></i></h4>
-        <ul className="list-group">
-          <li className="list-group-item">Email: {email}</li>
-          <li className="list-group-item">Phone: {phone}</li>
-        </ul>
+        {showContactInfo ? (
+          <React.Fragment>
+            <h4>
+              {name}{' '}
+              <i
+                onClick={this.onShowClick}
+                className="fa fa-angle-double-down"
+              />
+            </h4>
+            <ul className="list-group">
+              <li className="list-group-item">Email: {email}</li>
+              <li className="list-group-item">Phone: {phone}</li>
+            </ul>
+          </React.Fragment>
+        ) : (
+          <h4>
+            {name}{' '}
+            <i
+              onClick={this.onShowClick}
+              className="fa fa-angle-double-right"
+            />
+          </h4>
+        )}
       </div>
-    )
+    );
   }
 }
 
 Contact.propTypes = {
   contact: PropTypes.object.isRequired
-}
+};
 
-export default Contact
+export default Contact;
