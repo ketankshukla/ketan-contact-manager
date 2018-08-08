4ceb544b144d844f9f9f3390cf61e81492103d89
Ketan Shukla <ketankshukla@gmail.com>
2018-08-07 17:29:48 -0700

    Made the delete functionality work with the context, dispatch, provider and consumer

--- src/context.js
+++ src/context.js
@@ -2,6 +2,20 @@ import React, { Component } from 'react';
 
 const Context = React.createContext();
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'DELETE_CONTACT':
+      return {
+        ...state,
+        contacts: state.contacts.filter(
+          contact => contact.id !== action.payload
+        )
+      };
+    default:
+      return state;
+  }
+};
+
 export class Provider extends Component {
   state = {
     contacts: [
@@ -13,7 +27,10 @@ export class Provider extends Component {
         phone: '222-2222'
       },
       { id: 3, name: 'Bruce Dern', email: 'bdern@gmail.com', phone: '858-6666' }
-    ]
+    ],
+    dispatch: action => { 
+      this.setState(state => reducer(state,action))
+    }
   };
 
   render() {
